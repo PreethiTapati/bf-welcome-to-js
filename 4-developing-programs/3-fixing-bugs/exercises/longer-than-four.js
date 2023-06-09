9db3c2e9e660463cad8e1e44f5a485ed0307f14a
Preethi Tapati <118854537+PreethiTapati@users.noreply.github.com>
2023-06-09 13:43:34 +0200

    Update

--- 4-developing-programs/3-fixing-bugs/exercises/longer-than-four.js
+++ 4-developing-programs/3-fixing-bugs/exercises/longer-than-four.js
@@ -15,17 +15,27 @@
 
   --- experiments ---
 
-  1. FAILING:
-      EXPECT:
-      ACTUAL:
+  1. FAILING:cancel
+      EXPECT:'you cancelled'
+      ACTUAL:''
 
-    TRY:
-    PREDICT:
-    IT DID:
-    EXPLAIN:
+    TRY: try to delete the initiation of the  variable input inside the scope while (delete let before variable input)
+    PREDICT: should get expected output for cancel
+    IT DID:  got expected output for cancel, but still incorrect output for string
+    EXPLAIN: variable input should  be global
+
+  2. FAILING:abcde
+      EXPECT:'abcde'
+      ACTUAL: inside loop
+
+    TRY: change input to input.length for comparing input length
+    PREDICT:we get expected output for abcde
+    IT DID: we get expected output for abcde and over string longer than 4 characters
+    EXPLAIN:  we need to compare input length
 
   --- lessons learned ---
 
+  We need to be careful with variables inside a scope. should compare right type of values.
 
 */
 
